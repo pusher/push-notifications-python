c380ac22f03c928f7fb4b554f8cfbd1ccfeb5064
Jonathan Lloyd <jonathan@thisisjonathan.com>
2018-01-05 15:16:03 +0000

    Add endpoint override logic

--- pusher_push_notifications/__init__.py
+++ pusher_push_notifications/__init__.py
@@ -8,8 +8,23 @@ class PushNotifications(object):
     This client class can be used to publish notifications to the Pusher
     Push Notifications service"""
 
-    def __init__(self, instance_id, secret_key):
+    def __init__(self, instance_id, secret_key, endpoint=None):
         if not isinstance(instance_id, six.string_types):
             raise TypeError('instance_id must be a string')
         if not isinstance(secret_key, six.string_types):
             raise TypeError('secret_key must be a string')
+        if (endpoint is not None
+                and not isinstance(endpoint, six.string_types)):
+            raise TypeError('endpoint must be a string')
+
+        self.instance_id = instance_id
+        self.secret_key = secret_key
+        self._endpoint = endpoint
+
+    @property
+    def endpoint(self):
+        """Property method to calculate the correct Pusher API host"""
+        default_endpoint = '{}.pushnotifications.pusher.com'.format(
+            self.instance_id,
+        )
+        return self._endpoint or default_endpoint
--- tests/test_push_notifications.py
+++ tests/test_push_notifications.py
@@ -25,3 +25,32 @@ class TestPushNotifications(unittest.TestCase):
                 instance_id='1234',
                 secret_key=False,
             )
+
+    def test_constructor_should_fail_if_endpoint_not_string(self):
+        with self.assertRaises(TypeError):
+            PushNotifications(
+                instance_id='1234',
+                secret_key='1234',
+                endpoint=False,
+            )
+
+    def test_constructor_should_set_endpoint_default(self):
+        pn_client = PushNotifications(
+            instance_id='INSTANCE_ID',
+            secret_key='1234',
+        )
+        self.assertEqual(
+            pn_client.endpoint,
+            'INSTANCE_ID.pushnotifications.pusher.com',
+        )
+
+    def test_constructor_should_accept_endpoint_override(self):
+        pn_client = PushNotifications(
+            instance_id='INSTANCE_ID',
+            secret_key='1234',
+            endpoint='example.com/push',
+        )
+        self.assertEqual(
+            pn_client.endpoint,
+            'example.com/push',
+        )
