2b9fe0ee2c24650bb855bb34f1db04f4945fcdfb
Luka Bratos <luka@pusher.com>
2018-03-12 15:21:57 +0000

    Bump to 0.9.2

--- pusher_push_notifications/__init__.py
+++ pusher_push_notifications/__init__.py
@@ -6,7 +6,7 @@ import re
 import requests
 import six
 
-SDK_VERSION = '0.9.1'
+SDK_VERSION = '0.9.2'
 INTEREST_MAX_LENGTH = 164
 INTEREST_REGEX = re.compile('^(_-|=|@|,|\\.|:|[A-Z]|[a-z]|[0-9])*$')
 
--- tests/test_push_notifications.py
+++ tests/test_push_notifications.py
@@ -108,7 +108,7 @@ class TestPushNotifications(unittest.TestCase):
                 'content-type': 'application/json',
                 'content-length': '69',
                 'authorization': 'Bearer SECRET_KEY',
-                'x-pusher-library': 'pusher-push-notifications-python 0.9.1',
+                'x-pusher-library': 'pusher-push-notifications-python 0.9.2',
                 'host': 'instance_id.pushnotifications.pusher.com',
             },
         )
