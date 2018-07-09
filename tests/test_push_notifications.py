556e02778602e8d8b5ce47dd856c56ad8bca51ae
Jonathan Lloyd <jonathan@thisisjonathan.com>
2018-07-09 17:22:42 +0100

    Fail if instance_id/secret_key is empty

--- tests/test_push_notifications.py
+++ tests/test_push_notifications.py
@@ -29,6 +29,14 @@ class TestPushNotifications(unittest.TestCase):
             )
         self.assertIn('instance_id must be a string', str(e.exception))
 
+    def test_constructor_should_fail_if_instance_id_empty_string(self):
+        with self.assertRaises(ValueError) as e:
+            PushNotifications(
+                instance_id='',
+                secret_key='1234',
+            )
+        self.assertIn('instance_id cannot be the empty string', str(e.exception))
+
     def test_constructor_should_fail_if_secret_key_not_string(self):
         with self.assertRaises(TypeError) as e:
             PushNotifications(
@@ -36,6 +44,14 @@ class TestPushNotifications(unittest.TestCase):
                 secret_key=False,
             )
         self.assertIn('secret_key must be a string', str(e.exception))
+
+    def test_constructor_should_fail_if_secret_key_empty_string(self):
+        with self.assertRaises(ValueError) as e:
+            PushNotifications(
+                instance_id='1234',
+                secret_key='',
+            )
+        self.assertIn('secret_key cannot be the empty string', str(e.exception))
 
     def test_constructor_should_fail_if_endpoint_not_string(self):
         with self.assertRaises(TypeError) as e:
